01d559f8d137619c2bb24ff0e7b841f0c811d3f3
lroncevic <roncevicluka@outlook.com>
2023-02-05 17:45:37 +0100

    Added Specialties Map Imp. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.lukaroncevic.sfgpetclinic.services;
+
+import com.lukaroncevic.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
