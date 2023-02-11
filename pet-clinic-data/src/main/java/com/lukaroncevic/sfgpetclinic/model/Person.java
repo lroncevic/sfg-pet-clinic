206f19f9899167501610fa2767026f5f2917be28
lroncevic <roncevicluka@outlook.com>
2023-02-11 22:46:45 +0100

    Added Project Lombok to project. Closes #50.

--- pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/model/Person.java
@@ -2,29 +2,24 @@ package com.lukaroncevic.sfgpetclinic.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.*;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
-    }
-
-    public void setLastName(String lastName){
-        this.lastName = lastName;
-    }
 
 }
