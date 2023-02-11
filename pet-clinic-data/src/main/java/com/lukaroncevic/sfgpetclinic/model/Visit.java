2b70171799ea6787617517e4edca992475ef2897
lroncevic <roncevicluka@outlook.com>
2023-02-11 10:20:58 +0100

    Added Visit Entity. Closes #37.

--- pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package com.lukaroncevic.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "ped_id")
     private Pet pet;
 
     public LocalDate getDate() {
