52aeefe86f2edc87c5096410faa4fde9acece5d2
lroncevic <roncevicluka@outlook.com>
2023-03-03 18:41:43 +0100

    Added date pattern for Pet birthDate

--- pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/lukaroncevic/sfgpetclinic/model/Pet.java
@@ -2,6 +2,7 @@ package com.lukaroncevic.sfgpetclinic.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -24,7 +25,7 @@ public class Pet extends BaseEntity{
         this.birthDate = birthDate;
         if (visits != null && visits.size() > 0){
             this.visits = visits;
-    }
+        }
     }
 
     @Column(name = "name")
@@ -37,6 +38,7 @@ public class Pet extends BaseEntity{
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
